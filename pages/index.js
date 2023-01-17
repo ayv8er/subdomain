e309a96be31faa6894a4ee0ac4c5314409caba50
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-01-17 12:48:28 -0800

    creates sign in page layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
+import { useState } from "react";
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [user, setUser] = useState(null);
+
+  const login = () => {};
+
   return (
     <>
       <Head>
@@ -12,7 +17,32 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Wassup</div>
+      <div className="mt-32 flex justify-center ">
+        {!user && (
+          <div className="flex flex-col items-center w-11/12">
+            <h1 className="text-white font-bold text-3xl">
+              Wallet Not Connected
+            </h1>
+            <form className="flex flex-col items-center w-full">
+              <div className="mt-6 flex flex-col items-center w-1/3">
+                <label className="pb-4 text-2xl">
+                  Electronic Mail Address:
+                </label>
+                <input
+                  className="focus:outline-none bg-slate-700 rounded-md p-2 mx-2 w-full"
+                  type="email"
+                />
+              </div>
+              <button
+                onClick={login}
+                className="w-40 flex justify-center bg-gray-800 border-gray-700 text-white hover:bg-gray-700 active:bg-gray-500 border rounded-lg font-semibold text-xl mt-8 px-5 py-2.5"
+              >
+                Sign In
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
     </>
   );
 }
