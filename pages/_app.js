6294645787325658d055940ce8afb2c98d57d13a
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-01-18 08:54:02 -0800

    fixes app flow

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,9 @@
 import "@/styles/globals.css";
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import { UserContext } from "../lib/UserContext";
-import { magic } from "../lib/magic";
 
 export default function App({ Component, pageProps }) {
   const [user, setUser] = useState();
-  const router = useRouter();
-
-  useEffect(() => {
-    setUser({ loading: true });
-    magic.user.isLoggedIn().then((isLoggedIn) => {
-      if (isLoggedIn) {
-        magic.user.getMetadata().then((userData) => setUser(userData));
-        router.push("/profile");
-      } else {
-        setUser({ user: null });
-      }
-    });
-  }, []);
 
   return (
     <UserContext.Provider value={[user, setUser]}>
