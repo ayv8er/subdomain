2590a158c75a15ad9e0938e602c2cf5251dd5c55
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-01-17 16:31:33 -0800

    adds login endpoint

---
+++ pages/api/login.js
@@ -0,0 +1,15 @@
+import { Magic } from "@magic-sdk/admin";
+
+const magic = new Magic(process.env.MAGIC_SECRET_KEY);
+
+const login = async (req, res) => {
+  try {
+    const didToken = req.headers.authorization.substr(7);
+    await magic.token.validate(didToken);
+    res.status(200).json({ authenticated: true });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export default login;
--- pages/profile.js
+++ pages/profile.js
@@ -10,3 +10,5 @@ const Profile = () => {
     </div>
   );
 };
+
+export default Profile;
